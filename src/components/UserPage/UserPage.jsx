e400ac99ee9b2f386c28298355b0032f646b12a7
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-10-19 12:02:45 -0500

    cards on user page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -8,6 +8,8 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import CardMedia from '@mui/material/CardMedia';
 
 
 function UserPage() {
@@ -31,57 +33,49 @@ function UserPage() {
     </CardContent>
     </center>
   </Card>
+  <div style={{ display: 'flex', marginLeft: '120px', marginTop: '500px' }}>
+  <Card sx={{ maxWidth: 450, marginRight: '110px' }}>
+    <CardActionArea>
+      <CardMedia
+        component="img"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+  </Card>
 
-  </>
+  <Card sx={{ maxWidth: 450, marginRight: '20px' }}>
+    <CardActionArea>
+      <CardMedia
+        component="img"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Second card
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+  </Card>
+</div>
+</>
   )
 }
 
-// this allows us to use <App /> in index.js
+
 export default UserPage;
 
-
-/* <br />
-<br />
-    <div className="container">
-
-  <div className="grid">
-    <div className="grid-col grid-col_8">
-      <p>
-        With KwikInvoice, you can generate, customise and send out
-        professional invoices to your clients or customers superfast. The
-        best thing is that you get to do this at zero cost. Try it out now!
-      </p>
-
-      {/* <p>
-        Praesent consectetur orci dui, id elementum eros facilisis id. Sed
-        id dolor in augue porttitor faucibus eget sit amet ante. Nunc
-        consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut
-        finibus metus facilisis. Nullam eget lectus non urna rhoncus
-        accumsan quis id massa. Curabitur sit amet dolor nisl. Proin
-        euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed
-        lobortis augue mi vel felis. Duis ultrices sapien at est convallis
-        congue.
-      </p>
-
-      <p>
-        Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat.
-        Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In
-        vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed
-        sagittis neque id diam euismod, ut egestas felis ultricies. Nullam
-        non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
-        amet nisi.
-      </p> */
-//     </div>
-//     <div className="grid-col grid-col_4">
-//       <center>
-//       <br />
-//   <h2>Welcome, {user.username.charAt(0).toUpperCase() + user.username.slice(1)}!</h2>
-//   <p>Your ID is: {user.id}</p>
-//       </center>
-//     </div>
-//   </div>
-// </div>
-// <div>
-
-// </div>
-// ); */}
